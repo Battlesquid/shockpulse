0ca3b892890958933942bd7089c215ef032eabe7
Battlesquid <mayowae73@gmail.com>
2018-10-28 14:12:13 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
 var audio = document.getElementById('audio');
-audio.onload = function() {
-	audio.load();
-	audio.play();
-};
+var play = document.getElementById('play');
+play.addEventListener('click', function() {
+	audio.paused ? audio.play() : audio.pause();
+});
 
 var app = new PIXI.Application({
 	width: window.innerWidth,
@@ -13,19 +13,14 @@ var app = new PIXI.Application({
 });
 
 
-app.renderer.backgroundColor = 0x0083ff;
+app.renderer.backgroundColor = 0x2e3a6b;
+app.renderer.autoResize = true;
 document.body.appendChild(app.view);
-
-audio.addEventListener("canplaythrough", function() {
-	audio.play();
-}, false);
 
 
 var audioCtx = new AudioContext();
 var source = audioCtx.createMediaElementSource(audio);
 var analyser = audioCtx.createAnalyser();
-
-
 source.connect(analyser);
 analyser.connect(audioCtx.destination);
 
@@ -34,7 +29,6 @@ var bufferLength = analyser.frequencyBinCount;
 var dataArray = new Uint8Array(bufferLength);
 var WIDTH = app.screen.width;
 var HEIGHT = app.screen.height;
-
 var barWidth = (WIDTH / bufferLength);
 var barHeight;
 var rotation = 0;
@@ -46,8 +40,14 @@ for (var i = 0; i < bufferLength; i++) {
 	var g = new PIXI.Graphics();
 	g.pivot.set(app.screen.width / 2, app.screen.height / 2);
 	g.setTransform(app.screen.width / 2, app.screen.height / 2);
-	g.beginFill(0xe8e8e8);
-	g.drawRect(2, 2, 1, 1);
+	if (i % 2 === 0) {
+		g.beginFill(0xffffff);
+	}
+	else {
+		g.beginFill(0xcccccc);
+	}
+
+	g.drawRect(1, 1, 1, 1);
 	g.endFill();
 
 	app.stage.addChild(g);
@@ -67,7 +67,6 @@ container.pivot.set(app.screen.width / 2, app.screen.height / 2);
 container.position.set(app.screen.width / 2, app.screen.height / 2);
 app.stage.addChild(container);
 app.ticker.add(function(delta) {
-	x = 0;
 	analyser.getByteFrequencyData(dataArray);
 	graphics.clear();
 
@@ -78,11 +77,16 @@ app.ticker.add(function(delta) {
 		arr[i].rotation = (i * (Math.PI / 180));
 	}
 	graphics.beginFill(0xffffff);
-	graphics.drawCircle(0, 0, dataArray[4] / 4);
+	graphics.drawCircle(0, 0, dataArray[dataArray.length / 4] / 2);
 	graphics.endFill();
 
+	graphics.lineStyle(4, 0xe8e8e8, 0.2);
+	graphics.beginFill(0xffffff, 0);
+	graphics.drawCircle(0, 0, dataArray[0] + 200);
+	graphics.endFill();
 	container.rotation += delta * 0.005;
 });
-function map(value, lower1, upper1, lower2, upper2) {
-	return (value - lower1) / (upper1 - lower1) * (upper2 - lower2) + lower2;
-}
+
+// function map(value, lower1, upper1, lower2, upper2) {
+// 	return (value - lower1) / (upper1 - lower1) * (upper2 - lower2) + lower2;
+// }
