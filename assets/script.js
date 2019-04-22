caea00a46d282b8e84af0aa27fe03711ed7ab3ea
Battlesquid <mayowae73@gmail.com>
2019-04-22 11:18:19 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,98 +1,73 @@
-window.onload = () => {
-var audio = document.getElementById('audio');
-var play = document.getElementById('play');
-var file = document.getElementById('file');
-play.addEventListener('click', function() {
-	audio.paused ? audio.play() : audio.pause();
-});
+const audio = document.getElementById('audio');
+const play = document.getElementById('play');
+const file = document.getElementById('file');
+const cnv = document.querySelector('canvas');
+const progress = document.querySelector('#pI');
+const ctx = cnv.getContext('2d');
+let dataArray;
+audio.load();
 
-var app = new PIXI.Application({
-	width: window.innerWidth,
-	height: window.innerHeight,
-	antialias: true,
-	transparent: false,
-	resolution: 1
-});
+function update(a, w, t) {
+  progress.style.width = ((audio.currentTime / audio.duration) * 100) + '%';
 
-app.renderer.backgroundColor = 0x2E3A6B;
-app.renderer.autoResize = true;
-document.body.appendChild(app.view);
+  a.getByteFrequencyData(dataArray);
+  ctx.clearRect(0, 0, cnv.width, cnv.height);
+  ctx.fillStyle = '#fcfcfc';
+  ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-var audioCtx = new AudioContext();
-var analyser = audioCtx.createAnalyser();
-var source = audioCtx.createMediaElementSource(audio);
-var analyser = audioCtx.createAnalyser();
-source.connect(analyser);
-analyser.connect(audioCtx.destination);
-analyser.fftSize = 1024;
-var bufferLength = analyser.frequencyBinCount;
-var dataArray = new Uint8Array(bufferLength);
-var WIDTH = app.screen.width;
-var HEIGHT = app.screen.height;
-var barWidth = (WIDTH / bufferLength);
-var barHeight;
-var arr = [];
+  ctx.save();
+  ctx.translate(cnv.width / 2, cnv.height / 2);
+  ctx.rotate(t * 0.0020);
+  for (let i = 0; i < 360; i++) {
+    const h = dataArray[i];
+    ctx.fillStyle = `hsl(${t / 10}, 100%, 50%)`;
+    ctx.rotate(Math.round(i * Math.PI / 180));
+    ctx.fillRect(100, -5, h, 1);
+  }
+  ctx.restore();
 
-var graphics = new PIXI.Graphics();
-var container = new PIXI.Container();
-
-
-for (var i = 0; i < bufferLength; i++) {
-	var g = new PIXI.Graphics();
-	g.pivot.set(app.screen.width / 2, app.screen.height / 2);
-	g.setTransform(app.screen.width / 2, app.screen.height / 2);
-	if (i % 2 === 0) {
-		g.beginFill(0xffffff);
-	}
-	else {
-		g.beginFill(0xcccccc);
-	}
-	g.drawRect(1, 1, 1, 1);
-	g.endFill();
-	app.stage.addChild(g);
-	container.addChild(g);
-	arr.push(g);
+  t++;
+  requestAnimationFrame(function (ts) {
+    update(a, w, t)
+  });
 }
 
-graphics.pivot.set(app.screen.width / 2, app.screen.height / 2);
-graphics.setTransform(app.screen.width / 2, app.screen.height / 2);
-app.stage.addChild(graphics);
-container.addChild(graphics);
+cnv.width = window.innerWidth;
+cnv.height = window.innerHeight;
 
-container.pivot.set(app.screen.width / 2, app.screen.height / 2);
-container.position.set(app.screen.width / 2, app.screen.height / 2);
-app.stage.addChild(container);
-
-
-app.ticker.add(function(delta) {
-	analyser.getByteFrequencyData(dataArray);
-	graphics.clear();
-
-	for (var i = 0; i < bufferLength; i++) {
-		barHeight = dataArray[i];
-		arr[i].width = barWidth;
-		arr[i].height = barHeight;
-		arr[i].rotation = (i * (Math.PI / 180));
-	}
-	graphics.beginFill(0xffffff);
-	graphics.drawCircle(0, 0, dataArray[dataArray.length / 4] / 2);
-	graphics.endFill();
-
-	graphics.lineStyle(4, 0xe8e8e8, 0.2);
-	graphics.beginFill(0xffffff, 0);
-	graphics.drawCircle(0, 0, dataArray[0] + 200);
-	graphics.endFill();
-	container.rotation += delta * 0.005;
+play.addEventListener('click', function () {
+  audio.paused ? audio.play() : audio.pause();
 });
 
-function handleFiles(files) {
-	audio.src = URL.createObjectURL(files[0]);
-	document.getElementById('nowplaying').innerHTML = files[0].name.substring(0, files[0].name.indexOf(".mp3"));
-	try {
-		audio.play();
-	}
-	catch (error) {
-		console.error(error);
-	}
-}
+file.addEventListener('change', e => {
+  const files = e.target.files;
+  audio.src = URL.createObjectURL(files[0]);
+  document.getElementById('nowplaying').innerHTML = files[0].name.substring(0, files[0].name.indexOf(".mp3"));
+  audio.load();
+  audio.play();
+
+  const audioCtx = new AudioContext();
+  const analyser = audioCtx.createAnalyser();
+  const source = audioCtx.createMediaElementSource(audio);
+  source.connect(analyser);
+  analyser.connect(audioCtx.destination);
+  analyser.fftSize = 1024;
+
+  const bufferLength = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLength);
+
+  const barWidth = (cnv.width / bufferLength);
+
+  update(analyser, barWidth, 0);
+
+});
+
+document.querySelector('#title').addEventListener('click', e => {
+  document.querySelector('#about').classList.toggle('inactive');
+  e.target.classList.toggle('open');
+});
+
+window.onresize = _ => {
+  cnv.width = window.innerWidth;
+  cnv.height = window.innerHeight;
 }
